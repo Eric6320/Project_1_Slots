6437f1f344312e9a813dae04096fbbfda8747cf5
Eric Thompson <Eric Thompson>
2017-01-31 12:31:03 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ejtho/project_1_slots/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.ejtho.project_1_slots;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import static android.R.attr.onClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
